9b0f336c5867dc819ea4ca472b5ab16b99f23360
Evgeny <jenkneo.lal@gmail.com>
2024-12-10 20:05:57 +0400

    fix backend url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://ib-back.jenkneo.ru/api';
-export const API_URL = 'http://localhost:4000/api';
+export const API_URL = 'https://ib-back.jenkneo.ru/api';
+// export const API_URL = 'http://localhost:4000/api';
